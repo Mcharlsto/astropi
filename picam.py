d2759b0adc72ed24945c6bc4a33d481ac939c8cd
owencole12 <43905581+owencole12@users.noreply.github.com>
2019-02-03 13:51:43 +0000

    Add files via upload

---
+++ picam.py
@@ -0,0 +1,13 @@
+from time import sleep
+from picamera import PiCamera
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+camera = PiCamera()
+camera.resolution = (1296, 972)
+camera.start_preview()
+
+sleep(2)
+camera.capture(dir_path+"/image.jpg")
+
+os.remove(dir_path+"image.jpg")
