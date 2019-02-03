91d1a38249938b2d3e7350cdbd5ba11cb9d038af
Gigt1 <38812312+gigt1@users.noreply.github.com>
2019-02-03 14:50:36 +0000

    ../

--- humidity.py
+++ humidity.py
@@ -2,6 +2,7 @@ from sense_hat import SenseHat
 import time
 sense = SenseHat()
 sense.clear()
+timer = 0
 humiditystart = sense.get_humidity()
 while True:
     humiditycurrent = sense.get_humidity()
@@ -9,4 +10,10 @@ while True:
         humiditystart = sense.get_humidity()
         print("Higher")
         print(humiditycurrent)
+
+    elif timer > 10
+        timer = 0
+        humiditystart = sense.get_humidity()
+    else
+        timer = timer + 1
     time.sleep(1)
